d1630b9d111667b532879d9118a0416e7a686da2
jazz dhami <me@jazzdhami.com>
2020-06-28 20:43:46 +0530

    my question webpage

---
+++ app.js
@@ -0,0 +1,13 @@
+const question = document.querySelectorAll(".question");
+question.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
